24002fb89bd65f9aaafc9050919c4100dd0578a3
SauliusPadegimas <saulius.padegimas@gmail.com>
2022-12-30 11:55:10 +0200

    some errors

--- src/components/DateToAge.js
+++ src/components/DateToAge.js
@@ -1,8 +1,8 @@
 function DateToAge(dateString) {
-  var today = new Date();
-  var birthDate = new Date(dateString);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
+  const today = new Date();
+  const birthDate = new Date(dateString);
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     age--;
   }
